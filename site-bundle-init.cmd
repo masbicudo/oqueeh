1e02bd81de4b71e48894081dd6ccb8661bf08e76
Miguel Angelo <masbicudo@gmail.com>
2023-07-21 21:38:59 -0300

    Use incremental build

--- site-bundle-init.cmd
+++ site-bundle-init.cmd
@@ -2,4 +2,5 @@
 echo.source 'https://rubygems.org'>Gemfile
 echo.gem 'github-pages', group: :jekyll_plugins>>Gemfile
 call bundle add webrick
+call bundle add wdm
 call bundle install
--- site-serve.cmd
+++ site-serve.cmd
@@ -2,9 +2,9 @@
 setlocal
 set /a num=%random% + 4000
 if "%1"=="--" (
-    call bundle exec jekyll serve --port %num% --host 0.0.0.0
+    call bundle exec jekyll serve --port %num% --host 0.0.0.0 --incremental
 ) else (
-    start cmd /c bundle exec jekyll serve --port %num% --host 0.0.0.0
+    start cmd /c bundle exec jekyll serve --port %num% --host 0.0.0.0 --incremental
     ping 127.0.0.1 -n 9 > nul
     start http://127.0.0.1:%num%
 )
